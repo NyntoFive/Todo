2c574aa1eba360763515d345868e6de10b2da80b
Mike King <nyntofive@gmail.com>
2019-12-29 11:28:23 -0500

    Django To-Do List

---
+++ notes/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+LABEL_CHOICES = (
+    ("P", "primary"),
+    ("SE", "secondary"),
+    ("S", "success"),
+    ("D", "danger"),
+    ("D", "warning"),
+    ("I", "info"),
+    ("L", "light"),
+    ("D", "dark"),
+)
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    due_date = models.DateTimeField()
+    label = models.CharField(choices=LABEL_CHOICES, max_length=2)
+    finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
---
+++ notes/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Note
+
+
+def note_list_view(request):
+    to_do_list = Note.objects.filter(finished=False)
+    finished_list = Note.objects.filter(finished=True)
+    context = {"to_do_list": to_do_list, "finished_list": finished_list}
+    return render(request, "note_list.html", context)
+
+
+def finish_item(request, id):
+    note = get_object_or_404(Note, id=id)
+    note.finished = True
+    note.save()
+    return redirect("note_list")
+
+
+def recover_item(request, id):
+    note = get_object_or_404(Note, id=id)
+    note.finished = False
+    note.save()
+    return redirect("note_list")
+
+
+def delete_item(request, id):
+    note = get_object_or_404(Note, id=id)
+    note.delete()
+    return redirect("note_list")
+
